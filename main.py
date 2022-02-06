55ab3d1747a767fae87ce9f4058a148de6d7fecb
xllzevs <98533478+ZeVs012@users.noreply.github.com>
2022-02-06 12:31:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from pyrogram import Client, idle
 
-app = Client("my_account")
+app = Client("session_name") # для подключения доп.сессий нужно добавить:
+# app = Client2("session_name2")
 
 app.start()
+# app.start2()
 idle()
