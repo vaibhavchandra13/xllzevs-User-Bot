9bfd54ece4a740905518f4c76db68c6041206498
xllzevs <98533478+ZeVs012@users.noreply.github.com>
2022-02-06 12:59:23 +0500

    Update spam.py

--- modules/spam.py
+++ modules/spam.py
@@ -9,13 +9,13 @@ class Info:
     version = 1.7
     author = "@xllzevs"
     commands = {
-	    ".spam [количество сообщений] [text]": "Обычный спам сообщениями.",
-	    ".statspam [количество сообщений] [text]": "Обчный спам сообщениями с последующим удалением.",
-	    ".fastspam [количество сообщений] [text]": "Быстрый спам сообщениями.",
-	    ".slowspam [количество сообщений] [text]": "Медленный спам сообщениями."
+	    "spam [количество сообщений] [text]": "Обычный спам сообщениями.",
+	    "statspam [количество сообщений] [text]": "Обчный спам сообщениями с последующим удалением.",
+	    "fastspam [количество сообщений] [text]": "Быстрый спам сообщениями.",
+	    "slowspam [количество сообщений] [text]": "Медленный спам сообщениями."
     }
 
-@Client.on_message(filters.command("statspam", ".") & filters.me)
+@Client.on_message(filters.command("statspam", prefix) & filters.me)
 async def statspam(client: Client, message: Message):
     quantity = message.command[1]
     spam_text = " ".join(message.command[2:])
@@ -28,7 +28,7 @@ async def statspam(client: Client, message: Message):
         await asyncio.sleep(0.1)
 
 
-@Client.on_message(filters.command("spam", ".") & filters.me)
+@Client.on_message(filters.command("spam", prefix) & filters.me)
 async def spam(client: Client, message: Message):
     quantity = message.command[1]
     spam_text = " ".join(message.command[2:])
@@ -49,7 +49,7 @@ async def spam(client: Client, message: Message):
         await asyncio.sleep(0.15)
 
 
-@Client.on_message(filters.command("fastspam", ".") & filters.me)
+@Client.on_message(filters.command("fastspam", prefix) & filters.me)
 async def fastspam(client: Client, message: Message):
     quantity = message.command[1]
     spam_text = " ".join(message.command[2:])
@@ -70,7 +70,7 @@ async def fastspam(client: Client, message: Message):
         await asyncio.sleep(0.02)
 
 
-@Client.on_message(filters.command("slowspam", ".") & filters.me)
+@Client.on_message(filters.command("slowspam", prefix) & filters.me)
 async def slowspam(client: Client, message: Message):
     quantity = message.command[1]
     spam_text = " ".join(message.command[2:])
