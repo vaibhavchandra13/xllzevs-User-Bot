e3a89938b827ef46cdd96e8fe9ed4e3ec5bd5086
xllzevs <98533478+ZeVs012@users.noreply.github.com>
2022-02-06 12:35:49 +0500

    Update Bash.py

--- modules/Bash.py
+++ modules/Bash.py
@@ -6,12 +6,12 @@ class Info:
 	desc = "Выполнение bash кода с возвратом в телеграм."
 	author = "Неизвестен(переделал: @youngtitanium)"
 	version = "2.0"
-	commands = {".bash [код]": "Выполнение кода."}
+	commands = {"bash [код]": "Выполнение кода."}
 
 def bash(code: str):
 	return subprocess.getoutput(code)
 
-@Client.on_message(filters.me & filters.command("bash", "."))
+@Client.on_message(filters.me & filters.command("bash", prefix))
 async def bash_command(_, msg):
 	reply = msg.reply_to_message
 	
